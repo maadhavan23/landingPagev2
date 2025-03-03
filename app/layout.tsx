4f06326c2a1fcab3125379ab6c7e79f9b3ecfbac
Maadhavan Muthuselvan <maadhavanuga@gmail.com>
2025-03-03 08:27:59 -0500

    favicon!

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'MAADHAVAN',
+  description: 'Created with tears...',
+  generator: 'modulus.LLC',
 }
 
 export default function RootLayout({
@@ -14,6 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>{children}</body>
     </html>
   )
