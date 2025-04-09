742bac0da37507eb2ca762cb1e10b8ac02de75fe
Maadhavan Muthuselvan <maadhavanuga@gmail.com>
2025-04-09 13:50:59 -0400

    vercel PLEASSEE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
+
 
 export const metadata: Metadata = {
   title: 'MAADHAVAN',
@@ -18,7 +20,9 @@ export default function RootLayout({
         <link rel="icon" href="/logo.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body>{children}</body>
+      <body>{children}
+      <Analytics />
+      </body>
     </html>
   )
 }
